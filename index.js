1e4120bd598fd0aa31670c1379220e38cb016af3
Pawel Jurczynski <pawel.jurczynski@live.com>
2019-06-04 10:56:33 +0200

    add cache

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 80;
 
-app.get('/', async (req, res) => {
+let cache = [];
+
+async function writeCache() {
     const restaurants = [
         require('./emalia'),
         require('./ogrod'),
-        require('./szuwary'),
+        require('./szuwary')
     ];
 
-    const menu = await Promise.all([...restaurants.map(restaurant => restaurant.log())])
+    cache = await Promise.all(restaurants.map(r => r.log()))
+}
 
-    res.json(menu);
+setTimeout(writeCache, 900000);
 
+writeCache();
+
+app.get('/', async (req, res) => {
+    res.json(cache);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
