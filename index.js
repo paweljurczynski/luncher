152a2c5e7875c8732d6fd7f00559fe439df01be3
Pawel Jurczynski <pawel.jurczynski@live.com>
2019-06-04 11:27:04 +0200

    Add timezone

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 80;
-const moment = require("moment");
+const moment = require("moment-timezone");
 require("moment/locale/pl");
 
 const format = 'HH:mm';
+const timezone = 'Europe/Warsaw';
+const startTime = '09:30:00';
+const endTime = '12:00:00';
+const intervalTime = 15 * 60 * 1000;
+
+moment.tz.setDefault(timezone);
+
 let cache = {};
 
 async function writeCache() {
@@ -23,12 +30,12 @@ async function writeCache() {
 }
 
 setInterval(async () => {
-    const shouldWriteCache = moment().isBetween(moment('09:30:00', format), moment('12:00:00', format));
+    const shouldWriteCache = moment().isBetween(moment(startTime, format), moment(endTime, format));
 
     if (shouldWriteCache) {
         await writeCache();
     }
-}, 900000);
+}, intervalTime);
 
 writeCache();
 
